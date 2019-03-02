93ec87ef7bbf2edc8a4dcfce40e2e8f273beb214
manley <manleytalk@hotmail.com>
2019-03-02 22:54:30 +0800

    1.实现了LED屏显示 2.字符串字数统计 3.替换错误的字符串

--- IMissYouDemo/Program.cs
+++ IMissYouDemo/Program.cs
@@ -6,7 +6,23 @@ namespace IMissYouDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string contest = "安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！李红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！安红，我想你！";
+
+            Console.WriteLine(contest);
+
+            Console.WriteLine(contest.Length);
+
+            //替换前，检查
+            Console.WriteLine(contest.Contains("李红"));
+
+            contest = contest.Replace("李红", "安红");
+
+            Console.WriteLine(contest);
+
+            //替换后，检查
+            Console.WriteLine(contest.Contains("李红"));
+
+            Console.ReadKey();
         }
     }
 }
